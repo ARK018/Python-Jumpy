d7bfc0f95dff264ffbfb465460aef2277493f77a
Akshay <akshaythedesigner9@gmail.com>
2024-02-09 00:15:47 +0530

    First commit

---
+++ jumpy.py
@@ -0,0 +1,13 @@
+#import libraries
+import pygame
+
+#initialize pygame
+pygame.init()
+
+#game window dimensions
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+#create game window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
